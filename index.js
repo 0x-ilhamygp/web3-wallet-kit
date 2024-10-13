4829eea385843490e83dbe34a589d6da4941b682
0x-ilhamygp <154941494+0x-ilhamygp@users.noreply.github.com>
2024-10-13 19:34:23 +0700

    Update index.js
    feat: Export Web3 as service

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 const web3Service = require('./services/web3');
 const walletService = require('./services/wallet');
 
+const Web3 = web3Service.Web3;
+
 module.exports = {
   web3Service,
-  walletService
+  walletService,
+  Web3
 };
